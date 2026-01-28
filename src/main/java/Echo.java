2c9836e0bd95209b351134503293339e9bb4ae1f
Jairusljr <jairusleung12@gmail.com>
2026-01-28 20:03:08 +0800

    Implement interactive loop with exit command

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Echo {
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
 
         // Print Greeting
         System.out.println("____________________________________________________________");
@@ -7,7 +11,19 @@ public class Echo {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        while (true) {
+            line = in.nextLine();
+
+            if (line.equals("bye")) {
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(line);
+            System.out.println("____________________________________________________________");
+        }
         // Print Exit Message
+        System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
