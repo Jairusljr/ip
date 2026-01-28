7b47d2dc92a7cee136938a189db2458d7d39ad05
Jairusljr <jairusleung12@gmail.com>
2026-01-28 20:29:02 +0800

    Implement Add and List functionality

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -4,12 +4,15 @@ public class Echo {
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
+        String[] tasks  = new String[100];
+        int taskCount = 0;
+        String horizontalLine = "    ____________________________________________________________";
 
         // Print Greeting
-        System.out.println("____________________________________________________________");
+        System.out.println(horizontalLine);
         System.out.println(" Hello! I'm Echo");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(" What shall I add to the List for you?");
+        System.out.println(horizontalLine);
 
         while (true) {
             line = in.nextLine();
@@ -18,13 +21,24 @@ public class Echo {
                 break;
             }
 
-            System.out.println("____________________________________________________________");
-            System.out.println(line);
-            System.out.println("____________________________________________________________");
+            if (line.equals("list")) {
+                System.out.println(horizontalLine);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(horizontalLine);
+            } else {
+                tasks[taskCount] = line;
+                taskCount++;
+                System.out.println(horizontalLine);
+                System.out.println("added: " + line);
+                System.out.println(horizontalLine);
+            }
         }
+
         // Print Exit Message
-        System.out.println("____________________________________________________________");
+        System.out.println(horizontalLine);
         System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(horizontalLine);
     }
 }
