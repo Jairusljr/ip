a42eee25357d89c487a8833f3c1f69487de5749e
Jairusljr <jairusleung12@gmail.com>
2026-01-28 20:58:39 +0800

    Update main loop to support mark/unmark commands

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -4,7 +4,7 @@ public class Echo {
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
-        String[] tasks  = new String[100];
+        Task[] tasks  = new Task[100];
         int taskCount = 0;
         String horizontalLine = "    ____________________________________________________________";
 
@@ -27,8 +27,22 @@ public class Echo {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
                 System.out.println(horizontalLine);
+            } else if (line.startsWith("mark")) {
+                int index = Integer.parseInt(line.substring(5)) - 1;
+                tasks[index].markAsDone();
+                System.out.println(horizontalLine);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks[index]);
+                System.out.println(horizontalLine);
+            } else if (line.startsWith("unmark")) {
+                int index = Integer.parseInt(line.substring(7)) - 1;
+                tasks[index].unmarkAsDone();
+                System.out.println(horizontalLine);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks[index]);
+                System.out.println(horizontalLine);
             } else {
-                tasks[taskCount] = line;
+                tasks[taskCount] = new Task(line);
                 taskCount++;
                 System.out.println(horizontalLine);
                 System.out.println("added: " + line);
