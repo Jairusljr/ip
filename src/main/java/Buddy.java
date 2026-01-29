ea09ecce174f56c86b8097b9afa8ad59ecfe9b7c
JAIRUS\jairus <jairusleung12@gmail.com>
2026-01-29 15:13:58 +0800

    Upgrade Echo to Buddy and enhance UI feedback

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-public class Echo {
+public class Buddy {
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
         Task[] tasks  = new Task[100];
         int taskCount = 0;
-        String horizontalLine = "    ____________________________________________________________";
+        String horizontalLine = "____________________________________________________________";
 
         // Print Greeting
         System.out.println(horizontalLine);
-        System.out.println(" Hello! I'm Echo");
+        System.out.println(" Woof! I'm Buddy, your loyal Task-Tracker.");
         System.out.println(" What shall I add to the List for you?");
         System.out.println(horizontalLine);
 
@@ -31,21 +31,22 @@ public class Echo {
                 int index = Integer.parseInt(line.substring(5)) - 1;
                 tasks[index].markAsDone();
                 System.out.println(horizontalLine);
-                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("Awesome! I've checked this off your list:");
                 System.out.println(tasks[index]);
                 System.out.println(horizontalLine);
             } else if (line.startsWith("unmark")) {
                 int index = Integer.parseInt(line.substring(7)) - 1;
                 tasks[index].unmarkAsDone();
                 System.out.println(horizontalLine);
-                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("No problem, I've put this back on the list for you:");
                 System.out.println(tasks[index]);
                 System.out.println(horizontalLine);
             } else {
                 tasks[taskCount] = new Task(line);
                 taskCount++;
                 System.out.println(horizontalLine);
-                System.out.println("added: " + line);
+                System.out.println("Got it! I've added '" + line + "' to your pile.");
+                System.out.println("You now have " + (taskCount + 1) + " things on your list!");
                 System.out.println(horizontalLine);
             }
         }
