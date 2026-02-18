1a49f883a4001ec577c2fa03ae81a4fb4075a9a7
Jairusljr <jairusleung12@gmail.com>
2026-02-18 15:57:20 +0800

    Add getBy, getFrom, and getTo methods to Deadline and Event classes

--- src/main/java/buddy/task/Task.java
+++ src/main/java/buddy/task/Task.java
@@ -9,6 +9,14 @@ public class Task {
         this.isDone = false;
     }
 
+    public String getDescription() {
+        return this.description;
+    }
+
+    public boolean isDone() {
+        return this.isDone;
+    }
+
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
